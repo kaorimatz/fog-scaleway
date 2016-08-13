4d506cdc01d8fed49b065903b923dc4ecd440636
Satoshi Matsumoto <kaorimatz@gmail.com>
2016-08-13 16:37:43 +0900

    Filter bootscripts

--- lib/fog/scaleway/requests/compute/list_bootscripts.rb
+++ lib/fog/scaleway/requests/compute/list_bootscripts.rb
@@ -2,8 +2,8 @@ module Fog
   module Scaleway
     class Compute
       class Real
-        def list_bootscripts
-          get('/bootscripts')
+        def list_bootscripts(filters = {})
+          get('/bootscripts', filters)
         end
       end
     end
